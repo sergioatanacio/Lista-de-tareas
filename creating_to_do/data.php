9c03c859cc3f6f12619718491b98044ab7652bcd
sergio14camino <sergio14camino@gmail.com>
2022-04-11 07:39:38 -0500

    Ya funciona

--- creating_to_do/data.php
+++ creating_to_do/data.php
@@ -1,23 +1,13 @@
 <?php
 return array (
-  0 => 
-  array (
-    'primero' => 'Esto es',
-    'segundo' => 'El primer dato',
-  ),
   1 => 
   array (
-    'primero' => 'Vamos a ver',
-    'segundo' => 'una pelicula',
-  ),
-  2 => 
-  array (
-    'primero' => 'Insertando datos',
-    'segundo' => 'en una tabla',
+    'primero' => 'hey',
+    'segundo' => 'amigo',
   ),
   3 => 
   array (
-    'primero' => 'Tres',
-    'segundo' => 'Cuatro',
+    'primero' => 'Estamos',
+    'segundo' => 'Bien',
   ),
 );
--- creating_to_do/inicio.js
+++ creating_to_do/inicio.js
@@ -12,15 +12,17 @@ let data_table_fn = (datos)=>
     })
     .then( res => res.json())
     .then( data => {
+        console.log(data);
         let fragment = document.createDocumentFragment();
-        data.forEach((item, index, array) => {
-            template_table.querySelector('.primero').textContent = item.primero;
-            template_table.querySelector('.segundo').textContent = item.segundo;
-            template_table.querySelector('form input.index_to_delete').setAttribute('value', index);
+        for (let propiedad in data)
+        {
+            template_table.querySelector('.primero').textContent = data[propiedad].primero;
+            template_table.querySelector('.segundo').textContent = data[propiedad].segundo;
+            template_table.querySelector('form input.index_to_delete').setAttribute('value', propiedad);
 
             let clone_node = document.importNode(template_table, true);
             fragment.append(clone_node);
-        });
+        };
 
         let fragment_table_category = document.createDocumentFragment();
         clone_template_table_category = document.importNode(template_table_category, true);
@@ -45,11 +47,11 @@ data_table_fn(new FormData());
 let add_event_to_delete = ()=>{
     
     let form_delete_to_data = document.querySelectorAll('.form_delete_to_data');
-    form_delete_to_data.forEach((item, index, array) => {
+    form_delete_to_data.forEach((item) => {
 
         item.addEventListener('submit', (e) => {
             e.preventDefault();
-            let element_to_delete = new FormData(form_delete_to_data);
+            let element_to_delete = new FormData(item);
             fetch('api.php',{
                 method: 'POST',
                 body: element_to_delete,
